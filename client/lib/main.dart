4a15a89dd692e95d547b914eecd4cdecd509668f
Võ Trung Hoàng Hưng <21120011@student.hcmus.edu.vn>
2024-02-09 15:52:44 +0700

    add: setup firebase

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:client/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 
-void main() {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
