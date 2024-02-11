14f3f5893f741190060906aeb3f5c4363939bc92
Nguyễn Phúc Tân <21120028@student.hcmus.edu.vn>
2024-02-11 23:40:49 +0700

    connect firebase

--- server/server.go
+++ server/server.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"fmt"
+	"context"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
+	"google.golang.org/api/option"
+	firebase "firebase.google.com/go"
+	"firebase.google.com/go/auth"
 )
 
 func main() {
+	opt := option.WithCredentialsFile("server\configs\firstaid-b3f79-firebase-adminsdk-zdw1h-bf16551ec6.json")
+	app, err := firebase.NewApp(context.Background(), nil, opt)
+	if err != nil {
+		return nil, fmt.Errorf("error initializing app: %v", err)
+	}	
+
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
