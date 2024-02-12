c6208afd3c95b64827164fac138e0ce97cd271be
Võ Trung Hoàng Hưng <21120011@student.hcmus.edu.vn>
2024-02-12 13:38:36 +0700

    ui: gradient background for splash screen

--- client/lib/ui/greeting_screen.dart
+++ client/lib/ui/greeting_screen.dart
@@ -30,78 +30,90 @@ class GreetingScreen extends StatelessWidget {
     );
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: SafeArea(
-        child: Container(
-          color: Colors.transparent,
-          height: screen_size.height,
-          width: screen_size.width,
-          child: Column(
-            children: [
-              Container(
-                margin: EdgeInsets.only(left: 20.0, bottom: 20.0, top: 10.0),
-                  child: HeaderWidget()
-              ),
-              Gap(15),
-              Image.asset('assets/images/greeting_image.png'),
-              Gap(20),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 30.0),
-                child: Text(
-                  MyStrings.greeting_text,
-                  style: MyStyles.tinyTextStyle,
-                  textAlign: TextAlign.center,
+      body: Container(
+        height: screen_size.height,
+        width: screen_size.width,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: MyTheme.defaultGradientColors
+            // colors: MyTheme.orangeGiftGradientColors,
+          ),
+        ),
+        child: SafeArea(
+          child: Container(
+            color: Colors.transparent,
+            height: screen_size.height,
+            width: screen_size.width,
+            child: Column(
+              children: [
+                Container(
+                  margin: EdgeInsets.only(left: 20.0, bottom: 20.0, top: 10.0),
+                    child: HeaderWidget()
                 ),
-              ),
-              Gap(70),
-              Padding(
-                padding: const EdgeInsets.only(left: 30.0, right: 30.0),
-                child: SizedBox(
-                  width: double.infinity,
-                  child: FilledButton(
-                    onPressed: () => onTapLogin(context),
-                    style: FilledButton.styleFrom(
-                      elevation: 5,
-                      backgroundColor: Colors.deepOrangeAccent,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15.0),
-                      ),
-                      padding: EdgeInsets.symmetric(vertical: 12),
-                    ),
-                    child: const Text(
-                        'Login',
-                        style: MyStyles.tinyBoldTextStyle,
-                    ),
+                Gap(15),
+                Image.asset('assets/images/greeting_image.png'),
+                Gap(20),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 30.0),
+                  child: Text(
+                    MyStrings.greeting_text,
+                    style: MyStyles.tinyTextStyle,
+                    textAlign: TextAlign.center,
                   ),
                 ),
-              ),
-              Gap(10),
-              Padding(
-                padding: const EdgeInsets.only(left: 30.0, right: 30.0),
-                child: SizedBox(
-                  width: double.infinity,
-                  child: OutlinedButton(
-                    onPressed: () {  },
-                    style: OutlinedButton.styleFrom(
-                      elevation: 5,
-                      side: BorderSide(color: Colors.deepOrangeAccent, width: 1.0),
-                      foregroundColor: Colors.deepOrangeAccent,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(15.0),
+                Gap(70),
+                Padding(
+                  padding: const EdgeInsets.only(left: 30.0, right: 30.0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: FilledButton(
+                      onPressed: () => onTapLogin(context),
+                      style: FilledButton.styleFrom(
+                        elevation: 5,
+                        backgroundColor: Colors.deepOrangeAccent,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        padding: EdgeInsets.symmetric(vertical: 12),
                       ),
-                      padding: EdgeInsets.symmetric(vertical: 12),
-                    ),
-                    child: const Text(
-                      'Sign Up',
-                      style: TextStyle(
-                        color: Colors.deepOrangeAccent,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold
+                      child: const Text(
+                          'Login',
+                          style: MyStyles.tinyBoldTextStyle,
                       ),
                     ),
                   ),
                 ),
-              ),
-            ],
+                Gap(10),
+                Padding(
+                  padding: const EdgeInsets.only(left: 30.0, right: 30.0),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: OutlinedButton(
+                      onPressed: () {  },
+                      style: OutlinedButton.styleFrom(
+                        elevation: 5,
+                        side: BorderSide(color: Colors.deepOrangeAccent, width: 1.0),
+                        foregroundColor: Colors.deepOrangeAccent,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15.0),
+                        ),
+                        padding: EdgeInsets.symmetric(vertical: 12),
+                      ),
+                      child: const Text(
+                        'Sign Up',
+                        style: TextStyle(
+                          color: Colors.deepOrangeAccent,
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
