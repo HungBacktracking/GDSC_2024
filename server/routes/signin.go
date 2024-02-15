fceecf5fdf08792fe5dd55e532df337ec194fd1f
Nguyễn Phúc Tân <21120028@student.hcmus.edu.vn>
2024-02-15 20:18:33 +0700

    add sign in routes

---
+++ server/routes/signin.go
@@ -0,0 +1,10 @@
+package routes
+
+import (
+	"github.com/labstack/echo/v4"
+)
+
+func RegisterSigninRoutes(e *echo.Echo) {
+	// e.POST("/signin/login", controllers.GetUser)
+	// e.POST("/signin/get_profile", controllers.GetProfile)
+}
