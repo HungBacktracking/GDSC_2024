40de56b544fd54e16745247c1ee3da13e3e24c73
Võ Trung Hoàng Hưng <21120011@student.hcmus.edu.vn>
2024-02-16 16:00:40 +0700

    model: add address model for client

---
+++ client/lib/ui/sos_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class SOSScreen extends StatefulWidget {
+  const SOSScreen ({ super.key });
+
+  @override
+  State<SOSScreen> createState() => SOSScreenState();
+}
+
+class SOSScreenState extends State<SOSScreen> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
